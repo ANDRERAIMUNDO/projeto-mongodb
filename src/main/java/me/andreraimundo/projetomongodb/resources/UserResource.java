24e5adbf3d332ff5e87fbc2aa55be953d3a9495d
ANDRERAIMUNDO <9000andre@gmail.com>
2021-03-19 14:19:50 -0300

    User POST

--- src/main/java/me/andreraimundo/projetomongodb/resources/UserResource.java
+++ src/main/java/me/andreraimundo/projetomongodb/resources/UserResource.java
@@ -1,15 +1,17 @@
 package me.andreraimundo.projetomongodb.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import me.andreraimundo.projetomongodb.domain.User;
 import me.andreraimundo.projetomongodb.dto.UserDTO;
@@ -33,5 +35,14 @@ public class UserResource {
     public ResponseEntity <UserDTO> findByd(@PathVariable String id){
         User obj = userService.findById(id);    
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity <Void> insert(@RequestBody UserDTO objDto){ //@RequestBody passar um dto em uma requisição
+        User obj = userService.fromDTO(objDto); //converte para userDTO
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();//retorno 201 de criação
     }
 }
