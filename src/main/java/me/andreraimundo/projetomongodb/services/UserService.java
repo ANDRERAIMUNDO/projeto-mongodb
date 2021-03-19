9c8a4e959774b98d5a11bbbdeae4a76c2c6f8f0e
ANDRERAIMUNDO <9000andre@gmail.com>
2021-03-19 13:46:42 -0300

    Buscando User por ID e tratando erros de exeção

--- src/main/java/me/andreraimundo/projetomongodb/services/UserService.java
+++ src/main/java/me/andreraimundo/projetomongodb/services/UserService.java
@@ -1,18 +1,26 @@
 package me.andreraimundo.projetomongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import me.andreraimundo.projetomongodb.domain.User;
 import me.andreraimundo.projetomongodb.repository.UserRepository;
+import me.andreraimundo.projetomongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById (String id){
+       Optional<User> obj = userRepository.findById(id);
+       return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não foi encontrado"));
     }
 }
